77bb3a7d812b3acd2a2096fa46e6026bc3106dba
Apoorv Umang <apoorvumang@gmail.com>
2018-10-28 23:57:54 +0530

    changed plotting

--- plot_output_history.py
+++ plot_output_history.py
@@ -10,34 +10,43 @@ import json
 # importing the required module 
 import matplotlib.pyplot as plt 
 
-OUTPUT_HISTORY_FILE_NAME = 'output_history.txt'
-  
-f = open(OUTPUT_HISTORY_FILE_NAME, "r")
+OUTPUT_HISTORY_FOLDER = '../history/'
+NUM_CLASSES = 50
+MAX_EPOCHS = 20
 
-data = json.load(f)
 
-f.close()
 
-x = []
-y1 = []
-y2 = []
 
-for entry in data:
-	x.append(entry['epoch'])
-	y1.append(entry['loss'])
-	y2.append(entry['validation_loss'])
+for i in range(0, NUM_CLASSES):
+	OUTPUT_HISTORY_FILE_NAME =  OUTPUT_HISTORY_FOLDER + "output_history"+str(i)+".txt" 
+	f = open(OUTPUT_HISTORY_FILE_NAME, "r")
+	data = json.load(f)
+	f.close()
+	x = []
+	y1 = []
+	y2 = []
+	for entry in data:
+		x.append(entry['epoch'])
+		y1.append(entry['loss'])
+		y2.append(entry['validation_loss'])
+
+	plt.plot(x, y1) 
+	plt.plot(x, y2)
+	  
+	# naming the x axis 
+	plt.xlabel('Epochs') 
+	# naming the y axis 
+	plt.ylabel('Loss') 
+	  
+	# giving a title to my graph 
+	plt.title('Test and Validation loss over epochs') 
+	  
+	# function to show the plot 
+	plt.show() 
+
+# for entry in data:
+# 	x.append(entry['epoch'])
+# 	y1.append(entry['loss'])
+# 	y2.append(entry['validation_loss'])
   
 # plotting the points  
-plt.plot(x, y1) 
-plt.plot(x, y2)
-  
-# naming the x axis 
-plt.xlabel('Epochs') 
-# naming the y axis 
-plt.ylabel('Loss') 
-  
-# giving a title to my graph 
-plt.title('Test and Validation loss over epochs') 
-  
-# function to show the plot 
-plt.show() 
